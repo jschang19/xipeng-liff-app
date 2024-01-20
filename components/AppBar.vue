b28ad61db791b9e8d80ac9ad4bc0652abb776696
jschang19 <wwwsean13579@gmail.com>
2024-01-21 01:57:59 +0800

    style: adjust app bar padding

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-if="liffStore.isLoggedIn" class="w-full pb-3 h-12 bg-white fixed top-auto bottom-0 shadow-sm items-center flex justify-around sm:hidden">
-    <div v-for="(link, index) in links" :key="index" class="flex flex-col items-center justify-center">
+  <div v-if="liffStore.isLoggedIn" class="w-full h-14 bg-white fixed top-auto bottom-0 shadow-sm items-center flex justify-around sm:hidden">
+    <div v-for="(link, index) in links" :key="index" class="flex flex-col items-center justify-center py-3">
       <NuxtLink :to="link.link" class="flex flex-col items-center justify-center">
         <Component :is="link.icon" :class="activeLink === index ? 'text-gray-500' : 'text-black'" class="w-6 h-6" />
       </NuxtLink>
@@ -8,7 +8,7 @@
   </div>
 </template>
 <script setup lang="ts">
-import { HomeIcon, CornersIcon, ReaderIcon, Link2Icon } from "@radix-icons/vue";
+import { HomeIcon, CornersIcon, ReaderIcon, MixIcon } from "@radix-icons/vue";
 import { useLiffStore } from "~/stores/liff";
 const route = useRoute();
 const liffStore = useLiffStore();
@@ -26,7 +26,7 @@ const links = ref([
     icon: CornersIcon
   }, {
     link: "/links",
-    icon: Link2Icon
+    icon: MixIcon
   }
 ]);
 
