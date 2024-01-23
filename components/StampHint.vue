76f98a896bb63bcd2d10564243223f54ec3d8d46
jschang19 <wwwsean13579@gmail.com>
2024-01-23 13:10:46 +0800

    chore: add alert block

---
+++ components/StampHint.vue
@@ -0,0 +1,108 @@
+<template>
+  <Alert>
+    <AlertTitle>遊戲提示</AlertTitle>
+    <AlertDescription class="mt-3 space-y-1">
+      <div class="flex items-center">
+        <DotFilledIcon class="w-4 h-4 mr-1 inline-block" />
+        <div v-if="numToGetCoupon > 0">折價券：還差 {{ numToGetCoupon }} 個章</div>
+        <div v-else>
+          折價券：
+          <NuxtLink
+            to="/coupons"
+            class="underline font-medium"
+          >
+            可領取 🎉
+          </NuxtLink>
+        </div>
+      </div>
+      <div class="flex items-center">
+        <DotFilledIcon class="w-4 h-4 mr-1 inline-block" />
+        目前抽獎次數：{{ numLotteryChance }} 次
+      </div>
+      <div class="flex items-center">
+        <DotFilledIcon class="w-4 h-4 mr-1 inline-block" />
+        下一張抽獎券：還差 {{ numToGetLotteryChance.booth }} 個攤位章，{{
+          numToGetLotteryChance.speaker
+        }}
+        個講者章
+      </div>
+    </AlertDescription>
+  </Alert>
+</template>
+<script setup lang="ts">
+import { DotFilledIcon } from "@radix-icons/vue";
+import type { Stamp } from "@/types";
+
+// Stamp rules
+// 1. 折價券至少要有 3 個 stamp，種類不限
+// 2. 抽獎至少要有 3 個 stamp，至少要有 2 個 speaker stamp 跟 1 個 booth stamp
+// 3. 每 2 個 booth stamp 跟 1 個 speaker stamp 就可以抽獎一次，最多可以抽 3 次
+const COUPON_STAMP_REQUIRED = 3;
+const LOTTERY_STAMP_REQUIRED_BOOTH = 1;
+const LOTTERY_STAMP_REQUIRED_SPEAKER = 2;
+
+const props = defineProps<{
+  stamps: Stamp[];
+}>();
+
+const numToGetCoupon = computed(() => {
+  let num = 0;
+
+  for (const stamp of props.stamps) {
+    if (stamp.type !== "empty") {
+      num++;
+    }
+  }
+
+  if (num >= COUPON_STAMP_REQUIRED) {
+    return 0;
+  }
+
+  return COUPON_STAMP_REQUIRED - num;
+});
+
+// calculate how many lottery chance
+const numLotteryChance = computed(() => {
+  let numBooth = 0;
+  let numSpeaker = 0;
+
+  for (const stamp of props.stamps) {
+    if (stamp.type === "booth") {
+      numBooth++;
+    }
+
+    if (stamp.type === "speaker") {
+      numSpeaker++;
+    }
+  }
+
+  return Math.floor(
+    Math.min(numBooth / LOTTERY_STAMP_REQUIRED_BOOTH, numSpeaker / LOTTERY_STAMP_REQUIRED_SPEAKER)
+  );
+});
+
+// calculate how many stamps needed to get lottery chance
+// return two numbers: [booth, speaker]
+const numToGetLotteryChance = computed(() => {
+  let numBooth = 0;
+  let numSpeaker = 0;
+
+  for (const stamp of props.stamps) {
+    if (stamp.type === "booth") {
+      numBooth++;
+    }
+
+    if (stamp.type === "speaker") {
+      numSpeaker++;
+    }
+  }
+
+  return {
+    booth: Math.max(0, LOTTERY_STAMP_REQUIRED_BOOTH - (numBooth % LOTTERY_STAMP_REQUIRED_BOOTH)),
+    speaker: Math.max(
+      0,
+      LOTTERY_STAMP_REQUIRED_SPEAKER - (numSpeaker % LOTTERY_STAMP_REQUIRED_SPEAKER)
+    )
+  };
+});
+</script>
