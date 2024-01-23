b3a226d69e025ab8240428048ee20ba2162de148
jschang19 <wwwsean13579@gmail.com>
2024-01-23 13:16:36 +0800

    refactor: add stamp type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,27 +1,38 @@
 interface Profile {
-    userId: string;
-    displayName: string;
-    pictureUrl: string;
-    statusMessage?: string;
-    email: string | null;
-    type:{
-        staff: boolean;
-        speaker: boolean;
-    };
+  userId: string;
+  displayName: string;
+  pictureUrl: string;
+  statusMessage?: string;
+  email: string | null;
+  type: {
+    staff: boolean;
+    speaker: boolean;
+  };
 }
 
 interface Event {
-    id: string;
-    title: string;
-    type: "training" | "game";
-    isOpen: boolean;
-    repeatDay: 0 | 1 | 2 | 3 | 4 | 5 | 6;
-    createdBy: Profile;
-    createdAt: string;
-    startAt: string;
-    scope: 0 | 1;
-    place: string | null;
-    endAt: string;
+  id: string;
+  title: string;
+  type: "training" | "game";
+  isOpen: boolean;
+  repeatDay: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+  createdBy: Profile;
+  createdAt: string;
+  startAt: string;
+  scope: 0 | 1;
+  place: string | null;
+  endAt: string;
 }
 
-export { Profile, Event };
+interface Stamp {
+  id: string;
+  type: "speaker" | "booth" | "empty";
+  booth: {
+    name: string;
+    description: string;
+    imageUrl: string;
+    link: string;
+  };
+}
+
+export { Profile, Event, Stamp };
